e567f43229e8b1869bbfcaea89e9ded47298d755
Lucinda Davies <lucindagdavies@gmail.com>
2020-11-06 14:54:13 +0000

    changed table name

---
+++ app/models/fitness_class.py
@@ -0,0 +1,7 @@
+class Fitness_Class:
+
+    def __init__(self, name, time, duration, class_type, id=None):
+        self.name = name
+        self.duration = duration
+        self.class_type = class_type
+        self.id = id
